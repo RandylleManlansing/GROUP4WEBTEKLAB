896c5a266f14451e6b882665349673af75dd1eaf
seanpogi123 <williamsahagun27@gmail.com>
2017-04-12 22:20:50 +0800

    added new function to save added apparatus to the LS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function saveToLS(){
     var request = new XMLHttpRequest();
     request.open("GET", "file.json", true);
-    request.setRequestHeader("Content-type", "application/json");
+    request.setRequestHeader("Content-type", "application/json")
     request.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
             var response = this.responseText;
@@ -10,4 +10,22 @@ function saveToLS(){
         }
     };
         request.send();
+}
+
+// addition starts here
+// this is the function that saves the added apparatus to the local storage
+
+function saveApparatus() {
+
+            var form = document.getElementById("apparatusRecord");
+            var Atype = form.type.value; // maybe change this to a dropdown form? and or just add valdation for numerical value
+            var price = form.price.value;
+            var Aname = form.name.value;
+            var categ = form.categ.value;
+            var stock = form.stock.value;
+
+            var jsonString = " {  '"+Aname+"': { 'categ':'"+categ+"','type':'"+Atype+"',  'stock':'"+stock+"',  'price':'"+price+"' } } ";
+                jsonString = jsonString.replace(/'/g, "\"");
+        
+            localStorage.setItem(Aname, jsonString);
 }
